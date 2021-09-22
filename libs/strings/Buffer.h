39ad2ba7dad586dec9ae05768e09160d8044c1e9
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-09-23 00:41:57 +0200

    Update Buffer with some fixes

--- libs/strings/Buffer.h
+++ libs/strings/Buffer.h
@@ -46,8 +46,18 @@ namespace sfz
 class BufferCounter
 {
 public:
-    BufferCounter() = default;
-    ~BufferCounter()
+    /**
+     * @brief      Return the buffer counter object.
+     */
+    static BufferCounter& counter() noexcept
+    {
+        static BufferCounter counter;
+        return counter;
+    }
+
+protected:
+    BufferCounter() noexcept = default;
+    ~BufferCounter() noexcept
     {
 #ifndef NDEBUG
         std::cout << "Remaining buffers on destruction: " << numBuffers << '\n';
@@ -55,44 +65,33 @@ public:
 #endif
     }
 
-    void newBuffer(int size)
+public:
+    template <class I>
+    void newBuffer(I size) noexcept
     {
-        numBuffers++;
-        bytes.fetch_add(size);
+        ++numBuffers;
+        bytes.fetch_add(static_cast<size_t>(size));
     }
 
-    void bufferResized(int oldSize, int newSize)
+    template <class I>
+    void bufferResized(I oldSize, I newSize) noexcept
     {
-        bytes.fetch_add(newSize);
-        bytes.fetch_sub(oldSize);
+        bytes.fetch_add(static_cast<size_t>(newSize));
+        bytes.fetch_sub(static_cast<size_t>(oldSize));
     }
 
-    void bufferDeleted(int size)
+    template <class I>
+    void bufferDeleted(I size) noexcept
     {
-        numBuffers--;
-        bytes.fetch_sub(size);
+        --numBuffers;
+        bytes.fetch_sub(static_cast<size_t>(size));
     }
 
-    void bufferDeleted(size_t size)
-    {
-        bufferDeleted(static_cast<int>(size));
-    }
-
-    void bufferResized(size_t oldSize, size_t newSize)
-    {
-        bufferResized(static_cast<int>(oldSize), static_cast<int>(newSize));
-    }
-
-    void newBuffer(size_t size)
-    {
-        newBuffer(static_cast<int>(size));
-    }
-
-    int getNumBuffers() { return numBuffers; }
-    int getTotalBytes() { return bytes; }
+    size_t getNumBuffers() const noexcept { return numBuffers; }
+    size_t getTotalBytes() const noexcept { return bytes; }
 private:
-    std::atomic<int> numBuffers { 0 };
-    std::atomic<int> bytes { 0 };
+    std::atomic<size_t> numBuffers { 0 };
+    std::atomic<size_t> bytes { 0 };
 };
 
 
@@ -109,7 +108,7 @@ private:
  *
  * @tparam     Type       The buffer type
  * @tparam     Alignment  the required alignment in bytes (defaults to
- *                        SIMDConfig::defaultAlignment)
+ *                        config::defaultAlignment)
  */
 template <class Type, unsigned int Alignment = SIMDConfig::defaultAlignment>
 class Buffer {
@@ -129,9 +128,8 @@ public:
      * @brief Construct a new Buffer object that is empty
      *
      */
-    Buffer()
+    Buffer() noexcept
     {
-        counter().newBuffer(0);
     }
 
     /**
@@ -139,9 +137,8 @@ public:
      *
      * @param size
      */
-    Buffer(size_t size)
+    explicit Buffer(size_t size)
     {
-        counter().newBuffer(0);
         resize(size);
     }
 
@@ -153,52 +150,80 @@ public:
      * @return true if allocation succeeded
      * @return false otherwise
      */
-    bool resize(size_t newSize)
+    bool resize(size_t newSize, std::nothrow_t) noexcept
     {
         if (newSize == 0) {
             clear();
             return true;
         }
 
-        auto tempSize = newSize + 2 * AlignmentMask; // To ensure that we have leeway at the beginning and at the end
-        auto* newData = paddedData != nullptr ? std::realloc(paddedData, tempSize * sizeof(value_type)) : std::malloc(tempSize * sizeof(value_type));
+        Type* oldData = paddedData.get();
+        Type* oldNormalData = normalData;
+        std::size_t oldSize = alignedSize;
+
+        std::size_t tempSize = newSize + 2 * AlignmentMask; // To ensure that we have leeway at the beginning and at the end
+        Type* newData = reinterpret_cast<Type*>(std::calloc(tempSize, sizeof(value_type)));
         if (newData == nullptr) {
             return false;
         }
 
-        counter().bufferResized(largerSize * sizeof(value_type), tempSize * sizeof(value_type));
+        if (largerSize > 0)
+            counter().bufferResized(largerSize * sizeof(value_type), tempSize * sizeof(value_type));
+        else
+            counter().newBuffer(tempSize * sizeof(value_type));
+
         largerSize = tempSize;
         alignedSize = newSize;
-        paddedData = static_cast<pointer>(newData);
+        paddedData.release(); // realloc has invalidated the old pointer
+        paddedData.reset(static_cast<pointer>(newData));
         normalData = static_cast<pointer>(align(Alignment, alignedSize, newData, tempSize));
         normalEnd = normalData + alignedSize;
-		auto endMisalignment = (alignedSize & TypeAlignmentMask);
-		if (endMisalignment != 0)
+        std::size_t endMisalignment = (alignedSize & TypeAlignmentMask);
+        if (endMisalignment != 0)
             _alignedEnd = normalEnd + Alignment - endMisalignment;
         else
             _alignedEnd = normalEnd;
 
+        std::memcpy(normalData, oldNormalData, std::min(newSize, oldSize) * sizeof(Type));
+        std::free(oldData);
+
         return true;
+    }
+
+    /**
+     * @brief Resizes the buffer. Given that std::realloc may return either the same pointer
+     * or a new one, you need to account for both cases in the code if you are using deep pointers.
+     *
+     * @param newSize the new buffer size in bytes
+     */
+    void resize(size_t newSize)
+    {
+        if (!resize(newSize, std::nothrow))
+            throw std::bad_alloc();
     }
 
     /**
      * @brief Clear the buffers and frees the underlying memory
      *
      */
-    void clear()
+    void clear() noexcept
     {
-        counter().bufferResized(largerSize * sizeof(value_type), static_cast<size_t>(0));
-        largerSize = 0;
-        alignedSize = 0;
-        std::free(paddedData);
-        normalData = nullptr;
-        normalEnd = nullptr;
-        _alignedEnd = nullptr;
+        if (largerSize > 0)
+            counter().bufferDeleted(largerSize * sizeof(value_type));
+        _clear();
     }
-    ~Buffer()
+    ~Buffer() noexcept
     {
-        counter().bufferDeleted(largerSize * sizeof(value_type));
-        std::free(paddedData);
+        if (largerSize > 0)
+            counter().bufferDeleted(largerSize * sizeof(value_type));
+    }
+
+    /**
+     * @brief Get the size of the larger block which is actually allocated
+     */
+    size_t allocationSize() const noexcept
+    {
+        return largerSize;
     }
 
     /**
@@ -206,12 +231,10 @@ public:
      *
      * @param other
      */
-    Buffer(const Buffer<Type>& other)
+    Buffer(const Buffer<Type, Alignment>& other)
     {
-        counter().newBuffer(0);
-        if (resize(other.size())) {
-            std::memcpy(this->data(), other.data(), other.size() * sizeof(value_type));
-        }
+        resize(other.size());
+        std::memcpy(this->data(), other.data(), other.size() * sizeof(value_type));
     }
 
     /**
@@ -219,45 +242,43 @@ public:
      *
      * @param other
      */
-    Buffer(Buffer<Type>&& other) = delete;
-    // {
-    //     if (this != &other) {
-    //         counter().bufferDeleted(largerSize * sizeof(value_type));
-    //         std::free(paddedData);
-    //         largerSize = std::exchange(other.largerSize, 0);
-    //         alignedSize = std::exchange(other.alignedSize, 0);
-    //         paddedData = std::exchange(other.paddedData, nullptr);
-    //         normalData = std::exchange(other.normalData, nullptr);
-    //         normalEnd = std::exchange(other.normalEnd, nullptr);
-    //         _alignedEnd = std::exchange(other._alignedEnd, nullptr);
-    //     }
-    // }
+    Buffer(Buffer<Type, Alignment>&& other) noexcept
+        : largerSize(other.largerSize),
+          alignedSize(other.alignedSize),
+          normalData(other.normalData),
+          paddedData(std::move(other.paddedData)),
+          normalEnd(other.normalEnd),
+          _alignedEnd(other._alignedEnd)
+    {
+        other._clear();
+    }
 
-    Buffer<Type>& operator=(const Buffer<Type>& other)
+    Buffer<Type, Alignment>& operator=(const Buffer<Type, Alignment>& other)
     {
         if (this != &other) {
-            if (resize(other.size()))
-                std::memcpy(this->data(), other.data(), other.size() * sizeof(value_type));
+            resize(other.size());
+            std::memcpy(this->data(), other.data(), other.size() * sizeof(value_type));
         }
         return *this;
     }
 
-    Buffer<Type>& operator=(Buffer<Type>&& other) = delete;
-    // {
-    //     if (this != &other) {
-    //         counter().bufferDeleted(largerSize * sizeof(value_type));
-    //         std::free(paddedData);
-    //         largerSize = std::exchange(other.largerSize, 0);
-    //         alignedSize = std::exchange(other.alignedSize, 0);
-    //         paddedData = std::exchange(other.paddedData, nullptr);
-    //         normalData = std::exchange(other.normalData, nullptr);
-    //         normalEnd = std::exchange(other.normalEnd, nullptr);
-    //         _alignedEnd = std::exchange(other._alignedEnd, nullptr);
-    //     }
-    //     return *this;
-    // }
+    Buffer<Type, Alignment>& operator=(Buffer<Type, Alignment>&& other) noexcept
+    {
+        if (this != &other) {
+            if (largerSize > 0)
+                counter().bufferDeleted(largerSize * sizeof(value_type));
+            largerSize = other.largerSize;
+            alignedSize = other.alignedSize;
+            normalData = other.normalData;
+            paddedData = std::move(other.paddedData);
+            normalEnd = other.normalEnd;
+            _alignedEnd = other._alignedEnd;
+            other._clear();
+        }
+        return *this;
+    }
 
-    Type& operator[](size_t idx) { return *(normalData + idx); }
+    Type& operator[](size_t idx) noexcept { return *(normalData + idx); }
     constexpr pointer data() const noexcept { return normalData; }
     constexpr size_type size() const noexcept { return alignedSize; }
     constexpr bool empty() const noexcept { return alignedSize == 0; }
@@ -265,19 +286,27 @@ public:
     constexpr iterator end() const noexcept { return normalEnd; }
     constexpr pointer alignedEnd() const noexcept { return _alignedEnd; }
 
-
     /**
      * @brief      Return the buffer counter object.
      *
-     *             TODO: In C++ 17 all of this can be static inline.
-     *
      * @return     The buffer counter on which you can call various methods.
      */
-    static BufferCounter& counter()
+    static BufferCounter& counter() noexcept
     {
-        static BufferCounter counter;
-        return counter;
+        return BufferCounter::counter();
     }
+
+private:
+    void _clear()
+    {
+        largerSize = 0;
+        alignedSize = 0;
+        paddedData.reset();
+        normalData = nullptr;
+        normalEnd = nullptr;
+        _alignedEnd = nullptr;
+    }
+
 private:
     static constexpr int AlignmentMask { Alignment - 1 };
     static constexpr int TypeAlignment { Alignment / sizeof(value_type) };
@@ -286,20 +315,24 @@ private:
     static_assert(Alignment == 0 || Alignment == 4 || Alignment == 8 || Alignment == 16 || Alignment == 32, "Bad alignment value");
     static_assert(TypeAlignment * sizeof(value_type) == Alignment || !std::is_arithmetic<value_type>::value,
                   "The alignment does not appear to be divided by the size of the arithmetic Type");
-    void* align(std::size_t alignment, std::size_t size, void *&ptr, std::size_t &space )
+    void* align(std::size_t alignment, std::size_t size, void *ptr, std::size_t &space)
     {
-        std::uintptr_t pn = reinterpret_cast< std::uintptr_t>( ptr );
-        std::uintptr_t aligned = ( pn + alignment - 1 ) & - alignment;
+        std::uintptr_t pn = reinterpret_cast<std::uintptr_t>(ptr);
+        std::uintptr_t aligned = (pn + alignment - 1) & - alignment;
         std::size_t padding = aligned - pn;
-        if ( space < size + padding ) return nullptr;
+        if (space < size + padding) return nullptr;
         space -= padding;
-        return ptr = reinterpret_cast< void * >( aligned );
+        return reinterpret_cast<void *>(aligned);
     }
+
+    struct deleter {
+        void operator()(void *p) const noexcept { std::free(p); }
+    };
 
     size_type largerSize { 0 };
     size_type alignedSize { 0 };
     pointer normalData { nullptr };
-    pointer paddedData { nullptr };
+    std::unique_ptr<value_type[], deleter> paddedData;
     pointer normalEnd { nullptr };
     pointer _alignedEnd { nullptr };
     LEAK_DETECTOR(Buffer);
